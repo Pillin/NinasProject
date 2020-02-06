7ac1c6d987a51369d78c22440271e2831195d6d2
antolabarca <anto.labarca.s@gmail.com>
2020-02-06 14:03:36 -0300

    cambios en el modelo de las clases para que se guarde la hora de la clase además del día, y que la hora y el curso sean unique together

--- clases/models.py
+++ clases/models.py
@@ -9,11 +9,12 @@ class Clase(models.Model):
     nombre = models.CharField('nombre clase', max_length=100, default="clase_default")
     problemas = models.ManyToManyField(Problema, related_name="profesoras", blank=True)
     publica = models.BooleanField('clase publica', default=True)
-    fecha_clase = models.DateField("fecha clase", default=date.today)
+    fecha_clase = models.DateTimeField("fecha clase", auto_now_add=True)
     curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
 
     class Meta:
         unique_together = (("nombre", "curso"))
+        unique_together = (("fecha_clase", "curso"))
 
     def __str__(self):
         return self.nombre
